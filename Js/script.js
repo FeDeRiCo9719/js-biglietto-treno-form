b40c0f18ea1c46611f270240ee3081c358c3f8cf
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-08-30 15:28:17 +0200

    basic structure html

---
+++ Js/script.js
@@ -0,0 +1,27 @@
+/* Il programma dovrà mostrare una form da compilare con cui chiedere all'utente 
+il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina 
+(formattato con massimo due decimali, per indicare centesimi sul prezzo). 
+Questo richiederà un minimo di ricerca. */
+
+//-------------------------------------------------------------------------------
+
+// PROGRAMMA 
+// 1. seleziono il bottone
+
+// 2. al click del bottone genera 
+
+// inserire la logica di programmazione per generare il biglietto
+
+// 3. raccolgo i valori dei campi input e select all'interno di variabili
+
+// 4. calcolo il prezzo del biglietto
+
+// 5. al click del bottone annulla
+// nascondere il biglietto
+// svuotare i campi 
